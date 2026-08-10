4d3853432d47a934489af445f1daa53d6aa45fe6
Mayank Joshi <mayank.joshi@nagarro.com>
2026-08-10 20:32:29 +0530

    Initial commit

---
+++ src/test/java/com/mayank/reports_management/ReportsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayank.reports_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
