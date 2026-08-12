c6dcc5e2a3f28bf2614b690c630931d0c4e46ddc
Mayank Joshi <mayank.joshi@nagarro.com>
2026-08-12 13:31:46 +0530

    remove unwanted repository annotation

--- src/main/java/com/mayank/reports_management/repository/DepartmentRepository.java
+++ src/main/java/com/mayank/reports_management/repository/DepartmentRepository.java
@@ -2,9 +2,7 @@ package com.mayank.reports_management.repository;
 
 import com.mayank.reports_management.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
     Department findByName(String name);
